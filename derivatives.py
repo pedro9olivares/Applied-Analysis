e7a911f3f752c87f1716552442733caf0067c23d
Pedro Olivares <pedro9olivares@gmail.com>
2024-08-15 17:30:49 -0600

    Finished class 1

--- derivatives.py
+++ derivatives.py
@@ -1,21 +1,34 @@
 import numpy as np
 import copy
 
-# Partial derivative of f with respect to x_i, evaluated on x0
-def partial(f, x0, i, h):
+# Partial derivative of f with respect to x_i, evaluated on x0. 
+# This one uses "Diferencia finita hacia adelante"
+def partial_forward(f, x0, i, h):
     n = len(x0)
     ei = np.zeros(n)
     ei[i] = 1
 
     return (f(x0 + h*ei) - f(x0))/h
 
-# Approximation of gradient via diferencia centrada
-def gradient(f, x0):
+# Partial derivative of f with respect to x_i, evaluated on x0. 
+# This one uses "Diferencia finita centrada"
+def partial_centered(f, x0, i, h):
+    n = len(x0)
+    ei = np.zeros(n)
+    ei[i] = 1
+
+    return (f(x0 + h*ei) - f(x0 - h*ei))/(2*h)
+
+# Approximation of gradient
+def gradient(f, x0, method='centrada'):
     h = 10 ** -5 # Our bound is the cube root of machine's epsilon (which is 10 ** -16)
     n = len(x0)
     grad = np.zeros(n)
 
     for i in range(n):
-        grad[i] = partial(f, x0, i, h)
+        if method == 'adelante':
+            grad[i] = partial_forward(f, x0, i, h)
+        elif method == 'centrada':
+            grad[i] = partial_centered(f, x0, i, h)
 
     return grad
--- tests.py
+++ tests.py
@@ -1,10 +1,41 @@
 import functions
 from derivatives import gradient
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 # Evaluating rosenbrock's function at (1,1), which we KNOW is a local minimum
 f = functions.rosenbrock
 x0 = np.array([1.,1.])
 
-print(f(x0))
-print(gradient(f, x0))
+print('========================')
+print('Rosenbrock\'s function (f)')
+print('========================')
+
+print(f'\nf({x0}) = {f(x0)}')
+grad = gradient(f,x0,'adelante')
+print(f'\n∇f({x0})={grad} via forward differences...')
+grad = gradient(f,x0,'centrada')
+print(f'\n∇f({x0})={grad} via centered differences...')
+
+# Now, let's plot on the interval [0,2]x[0,2]
+m = 50 # Density of axes
+x_axis = np.linspace(0, 2, m)
+y_axis = np.linspace(0, 2, m)
+F = np.zeros((m,m))
+
+for i in range(m):
+    x0 = x_axis[i]
+    for j in range(m):
+        y0 = y_axis[j]
+        w = np.array([x0, y0])
+        F[i, j] = f(w)
+
+[X, Y] = np.meshgrid(x_axis, y_axis)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X,Y,F, cmap='viridis')
+
+plt.show()
+
+
