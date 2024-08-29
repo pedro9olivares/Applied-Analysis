890535e9886214ebe7d3b689663876831709ee7e
Pedro Olivares <pedro9olivares@gmail.com>
2024-08-29 17:29:12 -0600

    Fix max descent!!!

--- descentMethods.py
+++ descentMethods.py
@@ -92,3 +92,44 @@ def newton_descent(f, xi, maxiter = 50):
         k += 1
 
     return xi, k
+
+# Function that implements Newton's descent method that works on hessian with negative eigenvalues
+def newton_descent_unrestricted(f, xi, maxiter = 50):
+    tol = 10 ** -5
+    k = 0   # Iteration counter
+    
+    c1 = 0.1
+
+    grad = gradient(f, xi)
+    grad_norm = np.linalg.norm(grad)
+    
+    while grad_norm > tol and k < maxiter:
+        B = hessian(f, xi)
+        v = np.linalg.eig(B)
+        v_min = min(v)
+        if v_min < 0:
+            mu = -(v_min - 1)
+            B = B + mu*np.eye(len(xi))
+        p = np.linalg.solve(B, -grad) # Newtons system
+        a = 1
+
+        fx = f(xi)
+        slope = c1*(np.dot(grad, p))
+
+        xi = xi + a*p
+        fxi = f(xi)
+        ka_max = 10
+        ka = 0
+        while fxi > fx + a*slope and ka < ka_max:
+            a = a/2
+            xi = xi + a*p
+            fxi = f(xi)
+            ka += 1
+            
+        # xi = xi + a*p
+        grad = gradient(f, xi)
+        grad_norm = np.linalg.norm(grad)
+
+        k += 1
+
+    return xi, k
