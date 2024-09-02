6f6dd7b313c8b79a8fd8d55883a74353e63b3e30
Pedro Olivares <pedro9olivares@gmail.com>
2024-09-01 23:30:24 -0600

    Added ackley's function to functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 # Rosenbrock's function in two dims f(x,y)
 def rosenbrock(x):
@@ -29,6 +30,23 @@ def uneven_paraboloid(x):
     x2 = x[1]
 
     fx = x1**2 +10*x2**2
+    return fx
+
+# Implementación de la función multimodal de Ackley
+# Del artículo "Test functions for optimization needs",
+# por Marcin Molga, Czeslaw Smutnicki
+def ackley(x, a=20, b=0.2, c=2*math.pi):
+    sum1 = 0
+    sum2 = 0
+    n = len(x)
+
+    for i in range(n):
+        sum1 += x[i] ** 2
+
+    for i in range(n):
+        sum2 = math.cos(c * x[i])
+
+    fx = (-a) * math.exp((-b) * math.sqrt((1/n) * sum1)) - math.exp((1/n) * sum2) + a + math.exp(1)
     return fx
 
 
